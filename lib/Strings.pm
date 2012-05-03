5207cd7c48100e1576e145a93981f67f3425a21d
Jesse Alama <jesse.alama@gmail.com>
2012-05-03 16:10:51 +0200

    Weird export.

--- lib/Strings.pm
+++ lib/Strings.pm
@@ -2,14 +2,11 @@ package Strings;
 
 use charnames qw(:full);
 use base qw(Exporter);
-our @EXPORT_OK = qw($EMPTY_STRING
-		    $SP
-		    $COLON
-		    $LF);
+our @EXPORT_OK = ('$EMPTY_STRING', '$SP', '$COLON', '$LF');
 use Readonly;
 
 # Strings
-Readonly my $EMPTY_STRING => q{};
+Readonly my $EMPTY_STRING => '';
 Readonly my $SP => q{ };
 Readonly my $COLON => q{:};
 Readonly my $LF => "\N{LF}";
