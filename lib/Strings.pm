b5877bd4342b6bafae0c011a968fff9a7d69cf21
Jesse Alama <jesse.alama@gmail.com>
2012-05-03 12:56:37 +0200

    Wrong export notation.

--- lib/Strings.pm
+++ lib/Strings.pm
@@ -1,15 +1,18 @@
 package Strings;
 
-use Exporter;
+use charnames qw(:full);
+use base qw(Exporter);
 our @EXPORT_OK = qw($EMPTY_STRING
 		    $SP
-		    $COLON);
+		    $COLON
+		    $LF);
 use Readonly;
 
 # Strings
 Readonly my $EMPTY_STRING => q{};
 Readonly my $SP => q{ };
 Readonly my $COLON => q{:};
+Readonly my $LF => "\N{LF}";
 
 1;
 __END__
