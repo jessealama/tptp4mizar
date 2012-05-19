da84fd81194e9cb07b117edbeaa1804e7e2fcae5
Jesse Alama <jesse.alama@gmail.com>
2012-05-19 16:54:19 +0200

    Start to support arbitrary TSTP derivations.

--- lib/TSTPDerivation.pm
+++ lib/TSTPDerivation.pm
@@ -16,12 +16,18 @@ use Readonly;
 use Regexp::DefaultFlags;
 use charnames qw(:full);
 use List::MoreUtils qw(none);
+use IPC::Run qw(harness);
 
 # Our stuff
+use FindBin qw($RealBin);
 use Utils qw(error_message
 	     is_valid_xml_file);
-Readonly my $SP => q{ };
+
+# Strings
 Readonly my $LF => "\N{LF}";
+Readonly my $EMPTY_STRING => q{};
+Readonly my $TPTP4X => 'tptp4X';
+Readonly my $SP => q{ };
 
 has 'background_theory' => (
     is => 'ro',
@@ -40,8 +46,90 @@ has 'path' => (
     reader => 'get_path',
 );
 
+# Directories
+Readonly my $STYLESHEET_HOME => "$RealBin/../xsl";
+Readonly my $TSTP_STYLESHEET_HOME => "${STYLESHEET_HOME}/tstp";
+Readonly my $MIZAR_STYLESHEET_HOME => "${STYLESHEET_HOME}/mizar";
+
+my @extensions_to_generate = ('voc', 'evl', 'dno', 'dco', 'wsx');
+
+my %directory_for_extension = (
+    'voc' => 'dict',
+    'evl' => 'text',
+    'wsx' => 'text',
+    'dno' => 'prel',
+    'dco' => 'prel',
+    'the' => 'prel',
+);
+
 sub to_miz {
-    confess error_message ('We do not yet support transforming arbitrary TSTP solutions to Mizar texts.  Sorry!', $LF, 'Please complain loudly to the maintainers.');
+    my $self = shift;
+    my $directory = shift;
+    my $options_ref = shift;
+
+    my %options = defined $options_ref ? %{$options_ref} : ();
+
+    my $path = $self->get_path ();
+
+    my $directory_full = File::Spec->rel2abs ($directory);
+
+    foreach my $subdir_name ('dict', 'prel', 'text') {
+	my $subdir = "${directory_full}/${subdir_name}";
+	mkdir $subdir
+	    or confess error_message ('Unable to make the \'', $subdir_name, '\' subdirectory of ', $directory_full, '.');
+    }
+
+    my $prel_subdir_full = "${directory_full}/prel";
+
+    foreach my $extension (@extensions_to_generate) {
+	my $subdir_name = $directory_for_extension{$extension};
+	my $subdir = "${directory}/${subdir_name}";
+
+	my @xsltproc_call = ('xsltproc',
+			     '--stringparam', 'article', 'article',
+			     '--stringparam', 'prel-directory', $prel_subdir_full);
+
+	if (defined $options{'shape'}) {
+	    my $shape = $options{'shape'};
+	    if ($shape eq 'nested') {
+		push (@xsltproc_call, '--stringparam', 'shape', 'nested');
+	    } elsif ($shape eq 'flat') {
+		push (@xsltproc_call, '--stringparam', 'shape', 'flat');
+	    } else {
+		confess error_message ('Unknown proof shape \'', $shape, '\'.');
+	    }
+	} else {
+	    push (@xsltproc_call, '--stringparam', 'shape', 'flat');
+	}
+
+	push (@xsltproc_call, "${TSTP_STYLESHEET_HOME}/tstp2${extension}.xsl");
+	push (@xsltproc_call, $path);
+
+	my $xsltproc_err = $EMPTY_STRING;
+	my $xsltproc_harness = harness (\@xsltproc_call,
+					'>', "${subdir}/article.${extension}",
+					'2>', \$xsltproc_err);
+
+	$xsltproc_harness->start ();
+	$xsltproc_harness->finish ();
+
+	my $xsltproc_exit_code = ($xsltproc_harness->results)[0];
+
+	if ($xsltproc_exit_code != 0) {
+	    confess error_message ('xsltproc did not exit cleanly when generating the .', $extension, ' file for', "\n", "\n", '  ', $path, "\n", "\n", 'The exit code was ', $xsltproc_exit_code, '. The error message was:',  "\n", "\n", '  ', $xsltproc_err);
+	}
+    }
+
+    my $pp_stylesheet = "${MIZAR_STYLESHEET_HOME}/pp.xsl";
+    my $wsx_path = "${directory}/text/article.wsx";
+    my $miz_path = "${directory}/text/article.miz";
+    my $xsltproc_status = system ("xsltproc $pp_stylesheet $wsx_path > $miz_path");
+    my $xsltproc_exit_code = $xsltproc_status >> 8;
+    if ($xsltproc_exit_code != 0) {
+	croak ('Error: xsltproc did not exit cleanly when generating the .miz file for', "\n", "\n", '  ', $path, "\n", "\n", 'The exit code was ', $xsltproc_exit_code);
+    }
+
+    return;
 }
 
 1;
