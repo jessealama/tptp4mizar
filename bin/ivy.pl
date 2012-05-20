b5ff41f3c8212c9f8050b0aeb11cb227482f611b
Jesse Alama <jesse.alama@gmail.com>
2012-05-20 19:33:29 +0200

    Merge the Prover9 clausification and the Ivy refutation proper.

--- bin/ivy.pl
+++ bin/ivy.pl
@@ -111,11 +111,151 @@ sub ensure_stuff_is_runnable {
     return;
 }
 
+sub equal_attributes {
+    my $attr_1 = shift;
+    my $attr_2 = shift;
+
+    if (defined $attr_1 && defined $attr_2) {
+	my $attr_1_name = $attr_1->nodeName;
+	my $attr_2_name = $attr_2->nodeName;
+
+	if ($attr_1_name eq $attr_2_name) {
+	    my $attr_1_value = $attr_1->getValue ();
+	    my $attr_2_value = $attr_2->getValue ();
+	    return ($attr_1_value eq $attr_2_value);
+	} else {
+	    return 0;
+	}
+    } elsif ((! defined $attr_1) && (! defined $attr_2)) {
+	return 1;
+    } else {
+	return 0;
+    }
+}
+
+sub equal_nodes {
+    my $node_1 = shift;
+    my $node_2 = shift;
+
+    my $node_1_name = $node_1->nodeName;
+    my $node_2_name = $node_2->nodeName;
+
+    if ($node_1_name eq $node_2_name) {
+	my @node_1_children = $node_1->childNodes;
+	my @node_2_children = $node_2->childNodes;
+	if (scalar @node_1_children == scalar @node_2_children) {
+	    my @node_1_attrs = $node_1->attributes;
+	    my @node_2_attrs = $node_2->attributes;
+	    if (scalar @node_1_attrs == scalar @node_2_attrs) {
+		my @attr_comparison = pairwise { equal_attributes ($a, $b) }
+		    @node_1_attrs, @node_2_attrs;
+		if (all { $_ ? 1 : 0 } @attr_comparison) {
+		    my @children_comparison = pairwise { equal_nodes ($a, $b) }
+			@node_1_children, @node_2_children;
+		    if (all { $_ ? 1 : 0 } @children_comparison) {
+			return 1;
+		    } else {
+			return 0;
+		    }
+		} else {
+		    return 0;
+		}
+	    } else {
+		return 0;
+	    }
+	} else {
+	    return 0;
+	}
+    } else {
+	return 0;
+    }
+}
+
+sub equal_formula_nodes {
+    my $node_1 = shift;
+    my $node_2 = shift;
+
+    (my $node_1_formula_proper) = $node_1->findnodes ('*[1]');
+    (my $node_2_formula_proper) = $node_2->findnodes ('*[1]');
+
+    return equal_nodes ($node_1_formula_proper,
+			$node_2_formula_proper);
+}
+
+my $render_tptp_stylesheet = "$RealBin/../xsl/tptp/render-tptp.xsl";
+
+sub merge_prover9_solutions {
+    my $original_problem_xml = shift;
+    my $clausification_xml = shift;
+    my $refutation_xml = shift;
+
+    # ignore the original problem, for now
+
+    my $normalized_clausification_xml = normalize_variables ($clausification_xml);
+    my $parser = XML::LibXML->new ();
+
+    my $clausification_doc = $parser->parse_string ($normalized_clausification_xml);
+    my $refutation_doc = $parser->parse_string ($refutation_xml);
+
+    my %refutation_to_clausification = ();
+    my @refutation_axiom_nodes
+	= $refutation_doc->findnodes ('tstp/formula[@status = "axiom"]');
+    my @clausification_nodes = $clausification_doc->findnodes ('tstp/formula');
+
+    # Look up each of the axioms appearing in the refutation to find
+    # out its name in the clausification
+    foreach my $refutation_axiom_node (@refutation_axiom_nodes) {
+	my $axiom_name = $refutation_axiom_node->findvalue ('@name');
+	my $matching_clausification_index = first_index
+	    { equal_formula_nodes ($_, $refutation_axiom_node) } @clausification_nodes;
+	if ($matching_clausification_index < 0) {
+	    carp 'We did not find a match for the clausification axiom \'', $axiom_name, '\'.';
+	} else {
+	    my $matching_clausification_node
+		= $clausification_nodes[$matching_clausification_index];
+	    my $matching_name = $matching_clausification_node->findvalue ('@name');
+	    $refutation_to_clausification{$axiom_name} = $matching_name;
+	}
+    }
+
+    # Rewrite justifications in the refutation part
+    my $replacements = $EMPTY_STRING;
+    foreach my $axiom_name (keys %refutation_to_clausification) {
+	my $clausification_name = $refutation_to_clausification{$axiom_name};
+	$replacements .= ",${axiom_name}:${clausification_name}";
+    }
+    $replacements = ($replacements eq $EMPTY_STRING ? ',' : $replacements) . ',';
+
+    my $rename_justifications_stylesheet = "$RealBin/../xsl/tstp/rename-justifications.xsl";
+
+    my $replaced_refutation
+	= apply_stylesheet ($rename_justifications_stylesheet,
+			    $refutation_xml,
+			    undef,
+			    {
+				'replacements' => $replacements,
+			    });
+
+    my $ignore_axioms_stylesheet = "$RealBin/../xsl/tptp/ignore-axioms.xsl";
+    my $axiom_free_refutation = apply_stylesheet ($ignore_axioms_stylesheet,
+						  $replaced_refutation);
+
+    my $clausification_tptp = apply_stylesheet ($render_tptp_stylesheet,
+						$normalized_clausification_xml);
+    my $refutation_tptp = apply_stylesheet ($render_tptp_stylesheet,
+					    $axiom_free_refutation);
+
+    return "${clausification_tptp}${refutation_tptp}";
+
+}
+
 process_commandline ();
 
 ensure_stuff_is_runnable ();
 
 my $problem_file = $ARGV[0];
+
+my $problem_xml = tptp_xmlize ($problem_file);
 
 my @tptp2X_call = ($TPTP2X, '-tstdfof', '-fprover9', '-q2', '-d-', $problem_file);
 my @prover9_call = ($PROVER9);
@@ -133,22 +273,26 @@ if ($opt_format eq 'lisp') {
     exit $?;
 }
 
+# Extract the clausification part of the proof
+
 my $prooftrans_xml = run_harness (\@prooftrans_xml_call, $prover9_expanded);
-
 my $prover9_to_tptp_stylesheet = "$RealBin/../xsl/prover9/prover92tptp.xsl";
-my $render_tptp_stylesheet = "$RealBin/../xsl/tptp/render-tptp.xsl";
+my $clausification_stylesheet = "$RealBin/../xsl/prover9/clausification.xsl";
+my $clausification_prooftrans_xml = apply_stylesheet ($clausification_stylesheet,
+						      $prooftrans_xml);
+my $clausification_tptp_xml = apply_stylesheet ($prover9_to_tptp_stylesheet,
+						$clausification_prooftrans_xml);
 
-my $prover9_tptp_xml = apply_stylesheet ($prover9_to_tptp_stylesheet,
-					 $prooftrans_xml);
+# fofify
+my $clausification_tptp = apply_stylesheet ($render_tptp_stylesheet,
+					    $clausification_tptp_xml);
 
-my $tptp = apply_stylesheet ($render_tptp_stylesheet,
-			     $prover9_tptp_xml,
-			     undef,
-			     {
-				 'tstp' => '1',
-			     }
-			 );
+# warn 'clausification tptp = ', $clausification_tptp;
 
+$clausification_tptp = tptp_fofify ($clausification_tptp);
+$clausification_tptp_xml = tptp_xmlize ($clausification_tptp);
+
+# Now extract the resolution proof proper
 
 my $ivy_to_tstp_script = "$RealBin/ivy2tstp.pl";
 
@@ -157,10 +301,16 @@ if (! can_run ($ivy_to_tstp_script)) {
     exit 1;
 }
 
-my @ivy_to_tstp_call = ($ivy_to_tstp_script);
-my $ivy_tstp = run_harness (\@ivy_to_tstp_call, $ivy_proof_object);
+my @ivy_to_tstp_call = ($ivy_to_tstp_script, '--format=xml');
+my $ivy_tstp_xml = run_harness (\@ivy_to_tstp_call, $ivy_proof_object);
 
-print $ivy_tstp;
+my $merged_solution = merge_prover9_solutions ($problem_xml,
+					       $clausification_tptp_xml,
+					       $ivy_tstp_xml);
+
+
+
+print $merged_solution;
 
 __END__
 
